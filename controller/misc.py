a1edee033b55432ca44eb5a88602dfed2670d348
christian_neuerer@web.de <Lu24ci02fer95>
2021-01-25 11:18:29 +0100

    cleanup misc

--- controller/misc.py
+++ controller/misc.py
@@ -4,6 +4,7 @@ import asyncio
 from multiprocessing import Process
 from functools import wraps
 
+
 def get_cursor():
     """
     Hole Datenbankcursor für shopdatenbank
@@ -20,37 +21,3 @@ def get_admin_cursor():
     """
     a = sqlite3.connect('database/admin', isolation_level=None)
     return a.cursor()
-
-
-def sync_simulate_read_message_for_xss():
-    # async read_message in sync umwandeln
-    return asyncio.run(async_simulate_read_message_for_xss())
-
-
-
-
-
-# class message_reader:
-#     def __init__(self):
-#         self.loop = asyncio.get_event_loop()
-#
-#     def try_read_message(self):
-#         async def run():
-#             # headless browser starten und seite aufrufen
-#             browser = await pyppeteer.launch()
-#             page = await browser.newPage()
-#             await page.goto('localhost:5000/user/profile/tickets')
-#             await page.screenshot({'path': 'test.png'})
-#             await browser.close()
-#         return self.loop.run_until_complete(run())
-
-
-
-async def async_simulate_read_message_for_xss():
-    # headless browser starten und seite aufrufen
-
-    await page.goto('localhost:5000/user/profile/tickets')
-    await page.screenshot({'path': 'test.png'})
-    await browser.close()
-    return 'done'
-
--- static/js/socketio.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    let socket = io();
-    socket.on('connect', function () {
-        socket.emit('my event', {data: 'connected!'});
-    });
-})
-
